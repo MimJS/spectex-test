75ac908fee3ba94b3f000a9e6f1dbdec0fd3c8cb
MikDevJS <mateevskiy2005@yandex.ru>
2022-01-28 21:53:20 +0300

    v3.0

--- src/lib/components/Avatar.js
+++ src/lib/components/Avatar.js
@@ -1,11 +1,13 @@
-const Avatar = ({ size = 48, overlay, src }) => {
+const Avatar = ({ size = 48, overlay, src, alt }) => {
   return (
     <div
       className={`Avatar ${overlay ? "Avatar--overlay" : null}`}
       style={{ height: size, width: size }}
     >
       {overlay ? <span className="Avatar--overlay-text">{overlay}</span> : null}
-      {src ? <img src={src} className="Avatar--img" /> : null}
+      {src ? (
+        <img src={src} className="Avatar--img" alt={alt ? alt : "avatar alt"} />
+      ) : null}
     </div>
   );
 };
